6df5abe7a8f6d821b98433861ebba30425922c70
Nuntapong <tumacsp@gmail.com>
2024-09-20 21:40:45 +0700

    update and link front to layout.html
    ไปนอนเล่นล่ะ

--- project_event_management/event_web/urls.py
+++ project_event_management/event_web/urls.py
@@ -5,10 +5,12 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path("", View_CreateActivity.as_view(), name="create_activity"),
-    path("activity/<int:activity_id>", View_Activity.as_view(), name="asdasd"),
-    path("index/", index.as_view(), name="asdasd"),
+    path("", ViewHome.as_view(), name="url_p_homepage"),
+    path("activity/", ViewActivity.as_view(), name="url_p_activitypage"),
+    path("activity/<int:activity_id>", View_Activity.as_view(), name="url_p_activity_detail"),
+    path("create_activity/", View_CreateActivity.as_view(), name="url_o_activity_form"),
 ]
 
+# setting for showing media on website
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- project_event_management/event_web/views.py
+++ project_event_management/event_web/views.py
@@ -1,22 +1,18 @@
 from django.shortcuts import render, redirect
-from .forms import CreateActivity_Form
-from .models import Activity, ActivityImage
-
-from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
 from django.views import View
 from django.db import transaction
-
+from .forms import *
+from .models import *
 from django.db.models import F, Q, Count, Value, Avg, Max, Min, Sum
 from django.db.models.functions import Length, Upper, Concat
-from django.db.models.lookups import GreaterThan
-import json, datetime
-from .forms import *
+import json
+import datetime
 
 class View_CreateActivity(View):
     def get(self, request):
         form = CreateActivity_Form()
-        return render(request, 'activity_form.html', {'form': form})
+        return render(request, 'o_activity_form.html', {'form': form})
 
     def post(self, request):
         form = CreateActivity_Form(request.POST, request.FILES)
@@ -24,8 +20,8 @@ class View_CreateActivity(View):
             activity = Activity.objects.create(
                 title=form.cleaned_data['title'],
                 close_register_date=form.cleaned_data['close_register_date'],
-                start_date= form.cleaned_data['start_date'],
-                due_date= form.cleaned_data['due_date'],
+                start_date=form.cleaned_data['start_date'],
+                due_date=form.cleaned_data['due_date'],
                 location=form.cleaned_data['location'],
                 platform=form.cleaned_data['platform'],
                 activity_type=form.cleaned_data['activity_type'],
@@ -33,29 +29,34 @@ class View_CreateActivity(View):
                 description=form.cleaned_data['description'],
                 contact=form.cleaned_data['contact'],
                 category=form.cleaned_data['category'],
-                organizer_id=7,
-                is_approve = "Approval",
+                organizer_id=7, #FIXED HERE THIS IS A MANUAL INSERT :D
+                is_approve="Approval",
             )
 
             images = request.FILES.getlist('upload_image')
             for image in images:
                 ActivityImage.objects.create(activity=activity, image_path=image)
-            return redirect('asdasd')
+                
+            return redirect('url_p_homepage')
         else:
             print(form.errors)
             form = CreateActivity_Form()
-            return render(request, 'activity_form.html', {'form': form})
-        
-class index(View):
+            return render(request, 'o_activity_form.html', {'form': form})
+
+class ViewHome(View):
     def get(self, request):
-        return render(request, 'index.html')
-    
+        return render(request, 'p_home.html')
+
+class ViewActivity(View):
+    def get(self, request):
+        return render(request, 'p_activity.html')
+
 class View_Activity(View):
     def get(self, request, activity_id):
         activity = Activity.objects.get(id=activity_id)
         activity_images = ActivityImage.objects.filter(activity=activity)
 
-        return render(request, 'show_activity.html', {
+        return render(request, 'p_activity_detail.html', {
             'activity': activity,
             'activity_images': activity_images
         })
